2c776dc90ea1a8f3a6b6e16e8c6c620c1f92f5de
Dennis Daume <daume.dennis@gmail.com>
2014-08-03 13:44:29 +0200

    Formatting

--- Lager/SettingsStorage.cs
+++ Lager/SettingsStorage.cs
@@ -11,8 +11,7 @@ using System.Threading.Tasks;
 namespace Lager
 {
     /// <summary>
-    /// The base class for every settings storage.
-    /// Provides methods for saving/retrieving settings.
+    /// The base class for every settings storage. Provides methods for saving/retrieving settings.
     /// </summary>
     public abstract class SettingsStorage : INotifyPropertyChanged
     {
@@ -22,11 +21,15 @@ namespace Lager
         private readonly string keyPrefix;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SettingsStorage"/> class.
+        /// Initializes a new instance of the <see cref="SettingsStorage" /> class.
         /// </summary>
-        /// <param name="keyPrefix">This value will be used as prefix for all settings keys.
-        /// It should be reasonably unique, so that it doesn't collide with other keys in the same <see cref="IBlobCache"/>.</param>
-        /// <param name="cache">An <see cref="IBlobCache"/> implementation where you want your settings to be stored.</param>
+        /// <param name="keyPrefix">
+        /// This value will be used as prefix for all settings keys. It should be reasonably unique,
+        /// so that it doesn't collide with other keys in the same <see cref="IBlobCache" />.
+        /// </param>
+        /// <param name="cache">
+        /// An <see cref="IBlobCache" /> implementation where you want your settings to be stored.
+        /// </param>
         protected SettingsStorage(string keyPrefix, IBlobCache cache)
         {
             if (String.IsNullOrWhiteSpace(keyPrefix))
@@ -45,10 +48,10 @@ namespace Lager
         public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
-        /// Loads every setting in this storage into the internal cache, or, if the value doesn't exist in the storage,
-        /// initializes it with its default value.
-        /// You dont HAVE to call this method, but it's handy for applications with a high number of settings
-        /// where you want to load all settings on startup at once into the internal cache and not one-by-one at each request.
+        /// Loads every setting in this storage into the internal cache, or, if the value doesn't
+        /// exist in the storage, initializes it with its default value. You dont HAVE to call this
+        /// method, but it's handy for applications with a high number of settings where you want to
+        /// load all settings on startup at once into the internal cache and not one-by-one at each request.
         /// </summary>
         public Task InitializeAsync()
         {
@@ -62,11 +65,15 @@ namespace Lager
         }
 
         /// <summary>
-        /// Gets the value for the specified key, or, if the value doesn't exist, saves the <paramref name="defaultValue"/> and returns it.
+        /// Gets the value for the specified key, or, if the value doesn't exist, saves the
+        /// <paramref name="defaultValue" /> and returns it.
         /// </summary>
         /// <typeparam name="T">The type of the value to get or create.</typeparam>
         /// <param name="defaultValue">The default value, if no value is saved yet.</param>
-        /// <param name="key">The key of the setting. Automatically set through the <see cref="CallerMemberNameAttribute"/>.</param>
+        /// <param name="key">
+        /// The key of the setting. Automatically set through the <see
+        /// cref="CallerMemberNameAttribute" />.
+        /// </param>
         protected T GetOrCreate<T>(T defaultValue, [CallerMemberName] string key = null)
         {
             if (key == null)
@@ -96,12 +103,15 @@ namespace Lager
         }
 
         /// <summary>
-        /// Overwrites the existing value or creates a new settings entry.
-        /// The value is serialized via the Json.Net serializer.
+        /// Overwrites the existing value or creates a new settings entry. The value is serialized
+        /// via the Json.Net serializer.
         /// </summary>
         /// <typeparam name="T">The type of the value to set or create.</typeparam>
         /// <param name="value">The value to be set or created.</param>
-        /// <param name="key">The key of the setting. Automatically set through the <see cref="CallerMemberNameAttribute"/>.</param>
+        /// <param name="key">
+        /// The key of the setting. Automatically set through the <see
+        /// cref="CallerMemberNameAttribute" />.
+        /// </param>
         protected void SetOrCreate<T>(T value, [CallerMemberName] string key = null)
         {
             if (key == null)
